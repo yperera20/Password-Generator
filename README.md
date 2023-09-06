be9a17f31f9226eb469a4b0ddf4a0293e8449379
yperera20 <buddhinperera@yahoo.com>
2023-09-06 22:19:27 +1000

    update-2

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Password-Generator
+
+A simple web-based password generator that allows users to generate secure passwords based on their criteria. This project includes HTML, CSS, and JavaScript to create an interactive user experience.
+
+## Features
+
+- Generate passwords with various criteria:
+  - Password length between 8 and 128 characters.
+  - Inclusion of lowercase and uppercase characters.
+  - Inclusion of numeric characters.
+  - Inclusion of special characters.
+- Input validation to ensure valid password criteria.
+- User-friendly interface.
+
+## Usage
+
+1. Open the password generator in a web browser.
+2. Click the "Generate Password" button to start the password generation process.
+3. Follow the prompts to select the desired criteria for your password:
+   - Enter the password length.
+   - Choose whether to include lowercase, uppercase, numeric, and special characters.
+4. The generated password will be displayed in the textarea.
+
+## Website :
+
+To access the password generator: 
+
+![Alt text](image.png)
+
+
+
--- script.js
+++ script.js
@@ -1,13 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Function to generate a secure password
+function generatePassword() {
+  // Define character sets for password criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+  // Prompt for password length
+  var passwordLength = prompt("Enter the length of the password (between 8 and 128 characters):");
+  passwordLength = parseInt(passwordLength);
+
+  // Validate password length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return "";
+  }
+
+  // Prompt for character types to include
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("You must select at least one character type to include in the password.");
+    return "";
+  }
+
+  // Build a character set based on selected criteria
+  var characterSet = "";
+  if (includeLowercase) characterSet += lowercaseChars;
+  if (includeUppercase) characterSet += uppercaseChars;
+  if (includeNumeric) characterSet += numericChars;
+  if (includeSpecial) characterSet += specialChars;
+
+  // Generate the password
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * characterSet.length);
+    password += characterSet.charAt(randomIndex);
+  }
+
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
